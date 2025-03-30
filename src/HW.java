e7a6cf6dd4a06aa296240c90ed69e90dac3eb055
BekKG <nightwing0_14@mail.ru>
2025-03-30 20:03:06 +0300

    HomeWork

---
+++ src/HW.java
@@ -0,0 +1,10 @@
+public class HW {
+    public static void HW(String[] args){
+        var friend = 19;
+        friend = friend * 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+        System.out.println("Ответ на задание 4 " + friend);
+    }
+}
